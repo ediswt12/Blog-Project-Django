6de06125371dd07927785412f65896561eb0b55f
Edile Memmedli <edisim7276@gmail.com>
2025-12-14 20:50:49 +0400

    Update models.py

--- posts/models.py
+++ posts/models.py
@@ -19,6 +19,12 @@ class Category(models.Model):
     def __str__(self):
         return self.title
 
+class About(models.Model):
+    title = models.CharField(max_length=200, default="Haqqımızda")
+    content = models.TextField()
+    
+    def __str__(self):
+        return self.title
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
@@ -30,8 +36,50 @@ class Post(models.Model):
     thumbnail = models.ImageField()
     categories = models.ManyToManyField(Category)
     featured = models.BooleanField()
+    likes = models.ManyToManyField(User, related_name='liked_posts', blank=True)
+    bookmarks = models.ManyToManyField(User, related_name='bookmarked_posts', blank=True)
+    views = models.IntegerField(default=0)  # Posta baxış sayı
+
 
     def __str__(self):
         return self.title
 
+    def total_likes(self):
+        return self.likes.count()
+    
+    def total_bookmarks(self):
+        return self.bookmarks.count()
+    
+    
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.name
+    
+    
+class Poll(models.Model):
+    question = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.question
+
+
+class Option(models.Model):
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE, related_name='options')
+    option_text = models.CharField(max_length=255)
+    votes = models.ManyToManyField(User, blank=True, related_name='poll_votes')
+
+    def __str__(self):
+        return self.option_text
+
+class Comment(models.Model):
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.post.title}"
