20ea5719d880e739cc57ee1d2b47541e371b9a5d
Edile Memmedli <edisim7276@gmail.com>
2025-12-14 20:50:12 +0400

    Update admin.py

--- posts/admin.py
+++ posts/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
-from .models import Author, Category, Post
+from .models import Author, Category, Post, Comment, About, Tag
 
 admin.site.register(Author)
 admin.site.register(Category)
 admin.site.register(Post)
+admin.site.register(Tag)
+admin.site.register(Comment)
+admin.site.register(About)
